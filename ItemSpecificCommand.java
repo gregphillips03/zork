28a32766f0978a240bb196b38ab9a814cd6bb91f
Greg Phillips <greg.phillips@aecom.com>
2016-11-04 19:53:26 -0400

    Updated JavaDoc for ItemSpecificCommand Class Components/Methods

--- ItemSpecificCommand.java
+++ ItemSpecificCommand.java
@@ -1,21 +1,36 @@
 
 /**
- * ItemSpecificCommand
+ * ItemSpecificCommand is an abstract extension of the Command Class
  * 
  * @author      William (Greg) Phillips
- * @version     Bork v3.0
+ * @version     Zork v1
  */
 public class ItemSpecificCommand extends Command
 {
     private String verb = ""; 
     private String noun = ""; 
     
+    /**
+     * Constructor for objects of Class ItemSpecificCommand
+     * 
+     * @param verb      Item's action verb as a String
+     * @param noun      Item's pairing message sent from CommandFactory as complete input
+     */
     ItemSpecificCommand(String verb, String noun)
     {
         this.verb = verb; 
         this.noun = noun; 
     }
     
+    /**
+     * execute
+     * Searches user's inventory and room for item. If found it attempts to pair it with the corresponding verb.
+     * 
+     * @return s    Returns paired message for specified verb / from inventory
+     * @return s    String message indicating user cannot perform indicated verb on said object / from inventory
+     * @return s    Returns paired message for specified verb / from room
+     * @return s    String message indicating user cannot perform indicated verb on said object / from room
+     */
     String execute()
     {
         String [] parts = noun.split(" "); 
