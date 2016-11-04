875fe7f370a49d96c88066f462198bee3c51886d
Greg Phillips <greg.phillips@aecom.com>
2016-11-04 19:22:20 -0400

    Updated JavaDoc for MovementCommand Class Components/Methods

--- MovementCommand.java
+++ MovementCommand.java
@@ -2,20 +2,32 @@ import java.util.*;
 import java.io.*; 
 
 /**
- * MovementCommand
+ * MovementCommand is an abstract extension of the Command Class
  * 
  * @author      William (Greg) Phillips
- * @version     Bork v3.0
+ * @version     Zork v1
  */
 public class MovementCommand extends Command
 {
     private String dir = ""; 
     
+    /**
+     * Constructor for objects of Class MovementCommand
+     * 
+     * @param dir   Direction as String to move user
+     */
     MovementCommand(String dir)
     {
         this.dir = dir; 
     }
     
+    /**
+     * execute
+     * Attempts to move user between Room objects
+     * 
+     * @return      String for successful movement
+     * @return      String for unsuccessful movement
+     */
     String execute() throws InterruptedException, FileNotFoundException
     {
         GameState gs = GameState.instance(); 
