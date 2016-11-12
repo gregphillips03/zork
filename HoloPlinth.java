0c0f8dacb5aef5f2e91f16c0163a78c3220eb594
Greg Phillips <greg.phillips@aecom.com>
2016-11-11 23:23:02 -0500

    Added Holoplinth class for specific interactions with Holoplinth

---
+++ HoloPlinth.java
@@ -0,0 +1,60 @@
+import java.util.*; 
+import java.io.*; 
+
+/**
+ * The HoloPlinth class handles special interactions with the holoplinth item. 
+ * 
+ * @author      William (Greg) Phillips
+ * @version     Zork v1.2
+ */
+public class HoloPlinth
+{
+    static HoloPlinth holoPlinth = new HoloPlinth();
+    static final String passPhrase = "golgotha"; 
+    static boolean isLocked = true; 
+    
+    /**
+     * Constructor for objects of class HoloPlinth. Singleton class controls creation
+     */
+    public HoloPlinth()
+    {
+        //exists to control object creation
+    }
+
+    /**
+     * Exists to control object creation.
+     * 
+     * @return      HoloPlinth object to work with. 
+     */
+    public static HoloPlinth instance()
+    {
+        return holoPlinth; 
+    }
+    
+    /**
+     * Interaction with user to unlock Holoplinth. Prompts user for passphrase and unlocks the holoplinth if guessed correctly. 
+     * 
+     * @return      String message to user
+     */
+    public String unlock()
+    {
+        Scanner scan = new Scanner(System.in);
+        GameState gs = GameState.instance(); 
+        String s = "Invalid Input"; 
+        
+        if(!isLocked)
+        {
+            s = "This holoplinth is already unlocked"; 
+            return s; 
+        }
+        System.out.println("Enter passphrase: "); 
+        String userCode = scan.nextLine(); 
+        if(userCode.equals(passPhrase))
+        {
+            isLocked = false; 
+            gs.setScore(25); 
+            s = "Input Accepted...\nHoloplinth unlocked."; 
+        }
+        return s; 
+    }
+}
