0b5761ff926183b5bd27be9d7c131930ba6e3b4b
cstaier <cstaier@mail.umw.edu>
2016-12-06 14:38:27 -0500

    Combat Added
    Works with AttackCommand to provide logic for Combat. Basic turn-based system for now.

--- Combat.java
+++ Combat.java
@@ -19,12 +19,46 @@ public class Combat
      * 
      * @param d   Denizen player has encountered
      */
-    boolean Combat(Denizen d)
+    Denizen target;
+    Item weapon;
+    int damageDoable = 0;
+    GameState gs = GameState.instance(); 
+    Combat(Denizen d, Item i)
     {
         //Insert Combat logic here
-        return true;
+        this.target = d;
+        this.weapon = i;      
+        /*return true;*/
     }
-    
+    String execute() throws InterruptedException{ 
+        int enemyHealth = this.target.getHealth();
+        System.out.println("You attack " + this.target.getName() + "(" + enemyHealth + " health) " + " with " + weapon.getPrimaryName());
+        if(this.isWeapon(this.weapon)){ //If Item is a weapon, damage possible is 15
+            damageDoable = 15;
+        }
+        else{ //If Item is not a weapon, damage is relative to its weight
+            damageDoable = this.weapon.getWeight();
+        }
+        
+        this.target.setHealth(enemyHealth - damageDoable);
+        if(this.target.getHealth() <= 0){
+            gs.getAdventurersCurrentRoom().removeNpc(this.target);
+            System.out.println("You have done " + damageDoable + " damage to your target.");
+            System.out.println("You have killed " + this.target.getName());
+        }
+        else{
+            int playerHealth = (int)gs.getHealth();
+            gs.setHealth(playerHealth - 5);
+            System.out.println("The " + this.target.getName() + " attacks, dealing 5 damage.");
+            if(gs.getHealth() <= 0){
+                Event e = new Event("die", this.weapon.getPrimaryName());
+                e.generateEvent();
+                return "";
+            }
+            
+        }
+        return "";
+    }
     /**
      * @return winText or lossText depending on results of Combat
      */
@@ -39,4 +73,12 @@ public class Combat
         return 100; //temp value
     }
    
+    boolean isWeapon(Item chosen){ //Checks if item used is a weapon
+        if(chosen.getPrimaryName().equals("boltpistol") || chosen.getPrimaryName().equals("chainsword")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
